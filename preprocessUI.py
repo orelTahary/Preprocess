30ee786d32f7d027bda49efd78dbea8cabde5d4a
MatanNoach <61862949+MatanNoach@users.noreply.github.com>
2020-08-19 12:03:29 +0300

    finished crosstalk input from user

--- preprocessUI.py
+++ preprocessUI.py
@@ -1,13 +1,15 @@
 import glob
+import os
 from tempfile import NamedTemporaryFile
 import shutil
 import csv
 import preprocess as pp
+import matplotlib.pyplot
 
 
 # files location = D:\Users\Matan\Downloads\preprocess files test\
 # The function gets a row as a dictionary and adds/updates the DB by it
-def updateDB(dict):
+def updateDB(dictionary):
     # sets all the fields of the Data Base
     fields = ["Animal", "Date", "lfp", "Bandpass", "median", "files recorded",
               "Bad electrodes", "Crosstalk", "possible spiking channels", "Neurons after sorting"]
@@ -22,21 +24,22 @@ def updateDB(dict):
         isUpdated = False
         for row in reader:
             # if the row exists in the DB, update it
-            if row["Animal"] == str(dict.get("Animal")) and row["Date"] == str(dict.get("Date")):
-                for key in dict.keys():
-                    row.update({key: dict.get(key)})
+            if row["Animal"] == str(dictionary.get("Animal")) and row["Date"] == str(dictionary.get("Date")):
+                for key in dictionary.keys():
+                    row.update({key: dictionary.get(key)})
                 isUpdated = True
             # write the row to the temp file
             writer.writerow(row)
         # if the row does not exist in the DB, add it
         if not isUpdated:
-            writer.writerow(dict)
+            writer.writerow(dictionary)
     # close the temp file and replace it in the DB folder
     tempFile.close()
     shutil.move(tempFile.name, DBPath)
     print("ThemisDB.csv has been updated")
 
 
+# The function converts wireless data to bin data and updates the DB
 def wirelessToDB():
     pp.wirelessToBin(inDir, inDir + "binNew\\", fileList, elecList)
     filesNum = fileList.__len__()
@@ -45,6 +48,7 @@ def wirelessToDB():
     print(str(filesNum) + " files has been transformed from wireless to bin successfully")
 
 
+# The function converts wireless bin data to lfp bin data and updates the DB
 def lfpToDB():
     fileFormat = '{0}Elec{1}' + rangeStr + '.bin'
     pp.binToLFP(inDir + "binNew\\", inDir + "binLFP\\", fileFormat, elecList)
@@ -53,6 +57,7 @@ def lfpToDB():
     print(str(elecList.__len__()) + " files has been transformed from lfp to bin successfully")
 
 
+# the function filters wireless bin data to bandpass bin data and updates the DB
 def bandpassToDB():
     fileFormat = "Elec{0}" + rangeStr + ".bin"
     pp.bandpass_filter(inDir + "binNew\\", inDir + "binBand\\", fileFormat, elecList)
@@ -61,8 +66,53 @@ def bandpassToDB():
     print(str(elecList.__len__()) + " files has been transformed from bandpass to bin successfully")
 
 
+# The function shows a single electrode data by path
+def showElectrode(path):
+    fig, axes = pp.plotBin(path, [10.05, 10.2])
+    fig.set_size_inches((30, 5))
+    matplotlib.pyplot.show(fig)
+
+
+# The function filters good/bad electrodes by user input.
+# Each electrode is shown to the user and he enters good/bad
+# The DB gets updated after
 def filterGoodBad():
-    pp.plot_channels(inDir, fileList, elecList, raw_fold='binNew')
+    badElecList = []
+    # Present all electrodes
+    for elec in elecList:
+        showElectrode(os.path.join(inDir + "binNew", "Elec" + str(elec) + rangeStr + ".bin"))
+        # Ask for an answer from the user
+        result = input("Enter good/bad")
+        if str(result) == "bad":
+            badElecList.append(elec)
+    # updates the bad electrodes
+    basicRow.update({"Bad electrodes": badElecList})
+    updateDB(basicRow)
+
+def displayInteraction():
+    ccr, ccf = pp.plot_corr_mat(inDir, rangeStr, elecList, raw_fold="binNew")
+
+
+# The function asks the user to analyze crosstalk of electrodes and asks him to enter them.
+# The DB gets updated after
+def crossTalkToDB():
+    result = input("Enter channels with crosstalk (a,b,c...) or write stop to finish: ")
+    lisOfNumLists = []
+    while result != "stop":
+        # split the result by ','
+        strList = result.split(',')
+        try:
+            # try to convert each string to an int
+            numList = list(map(lambda string: int(string), strList))
+            lisOfNumLists.append(numList)
+        except ValueError:
+            # if not all strings can be converted, print an error message
+            print("Not all values are numbers! Please enter again")
+        # ask for a user input again
+        result = input("Enter channels with crosstalk (a,b,c...) or write stop to finish: ")
+    # update the DB
+    basicRow.update({"Crosstalk": lisOfNumLists})
+    updateDB(basicRow)
 
 
 # inDir = input("Enter a Wireless Recordings path:")
@@ -77,7 +127,9 @@ elecList = list(range(2, 33))
 basicRow = {"Animal": "K6", "Date": "20/03/2020"}
 rangeStr = "-F{0}T{1}".format(fileList[0], fileList[-1])
 
-wirelessToDB()
-lfpToDB()
-bandpassToDB()
-filterGoodBad()
+# wirelessToDB()
+# lfpToDB()
+# bandpassToDB()
+# filterGoodBad()
+displayInteraction()
+# crossTalkToDB()
